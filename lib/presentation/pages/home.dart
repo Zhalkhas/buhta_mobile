069abeb7461b8cdfb7240295b8ad42e37e6fba01
Zhalkhas <jalgas1998@gmail.com>
2021-06-16 11:08:38 +0000

    added cards for home page

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,16 +1,34 @@
+import 'package:buhta/presentation/widgets/title_card.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/fontelico_icons.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<StatefulWidget> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TitleCard(
+              title: 'Товары и услуги',
+              color: Colors.red,
+              leadingIcon: Fontelico.emo_angry,
+              onTap: (){print('asdasd');},
+            ),
+            TitleCard(
+                title: 'Счета на оплату',
+                color: Color.fromRGBO(0, 0xc2, 0xe2, 1),
+                leadingIcon: FontAwesome5.money_bill),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/presentation/widgets/title_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TitleCard extends StatelessWidget {
+  final String title;
+  final Color color;
+  final VoidCallback onTap;
+  final IconData leadingIcon;
+
+  const TitleCard({
+    Key key,
+    @required this.title,
+    @required this.color,
+    @required this.leadingIcon,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+
+        height: size.height * 0.2,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.zero,
+        ),
+        child: Center(
+          child: ListTile(
+            //TODO: fix splash effect
+            onTap: onTap,
+            leading: Icon(
+              leadingIcon,
+              color: Colors.white,
+              size: 50,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.end,
+              style: GoogleFonts.openSans(
+                color: Colors.white,
+                textStyle: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
