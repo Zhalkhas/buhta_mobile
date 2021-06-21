180e07c284b8a1666b84022e311667232387efe3
Zhalkhas <jalgas1998@gmail.com>
2021-06-21 09:19:37 +0000

    fixes
    Took 32 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
-import 'package:buhta/presentation/pages/home.dart';
-import 'package:buhta/presentation/widgets/willpop_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'data/datasources/bill_local_datasource.dart';
+import 'data/datasources/product_local_datasource.dart';
+import 'data/models/bill_model.dart';
+import 'data/models/product_model.dart';
+import 'data/repos/bill_repo_impl.dart';
+import 'data/repos/product_repo_impl.dart';
+import 'domain/usecases/add_new_product.dart';
+import 'domain/usecases/edit_bill.dart';
+import 'domain/usecases/get_all_bills.dart';
+import 'domain/usecases/get_all_products.dart';
+import 'presentation/bloc/bills_list_cubit.dart';
+import 'presentation/bloc/product_list_cubit.dart';
+import 'presentation/pages/home.dart';
+import 'presentation/widgets/willpop_wrapper.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(ProductModelAdapter());
+  Hive.registerAdapter(BillModelAdapter());
+  Hive.registerAdapter(BillModelStatusAdapter());
   runApp(MyApp());
 }
 
@@ -24,56 +44,70 @@ class _MyAppState extends State<MyApp> {
           primarySwatch: _createMaterialColor(
             Color.fromRGBO(0x00, 0xc2, 0xe2, 1),
           ),
-          // MaterialColor(
-          //   0x00c2e2,
-          //   {
-          //     50: Color.fromRGBO(0x00,0xc2,0xe2, .1),
-          //     100: Color.fromRGBO(0x00,0xc2,0xe2, .2),
-          //     200: Color.fromRGBO(0x00,0xc2,0xe2, .3),
-          //     300: Color.fromRGBO(0x00,0xc2,0xe2, .4),
-          //     400: Color.fromRGBO(0x00,0xc2,0xe2, .5),
-          //     500: Color.fromRGBO(0x00,0xc2,0xe2, .6),
-          //     600: Color.fromRGBO(0x00,0xc2,0xe2, .7),
-          //     700: Color.fromRGBO(0x00,0xc2,0xe2, .8),
-          //     800: Color.fromRGBO(0x00,0xc2,0xe2, .9),
-          //     900: Color.fromRGBO(0x00,0xc2,0xe2, 1),
-          //   },
-          // ),
           textTheme: GoogleFonts.openSansTextTheme()),
       home: SafeArea(
         child: Scaffold(
           bottomNavigationBar: BottomNavigationBar(
+            elevation: 5,
             currentIndex: _index,
             items: [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
               BottomNavigationBarItem(
-                  icon: Icon(FontAwesome5.person_booth), label: ""),
+                  icon: Icon(FontAwesome5.user_circle), label: "Профиль"),
             ],
             onTap: (value) => setState(() => _index = value),
           ),
-          body: IndexedStack(
-            index: _index,
-            children: [
-              Navigator(
-                initialRoute: '/home',
-                onGenerateRoute: (settings) {
-                  switch (settings.name) {
-                    case '/home':
-                      return MaterialPageRoute(
-                        builder: (context) => WillPopWrapper(
-                          child: HomePage(),
+          body: Builder(
+            builder: (context) {
+              final productRepo =
+                  ProductRepositoryImpl(ProductLocalDataSource());
+              final billsRepo = BillsRepositoryImpl(BillLocalDataSource());
+              return MultiBlocProvider(
+                providers: [
+                  BlocProvider(
+                    create: (context) => ProductListCubit(
+                      GetAllProducts(productRepo),
+                      AddNewProduct(productRepo),
+                    )..getList(),
+                  ),
+                  BlocProvider(
+                    create: (context) => BillsListCubit(
+                      EditBill(billsRepo),
+                      GetAllBills(billsRepo),
+                    )..getList(),
+                  )
+                ],
+                child: IndexedStack(
+                  index: _index,
+                  children: [
+                    Navigator(
+                      initialRoute: '/home',
+                      onGenerateRoute: (settings) {
+                        switch (settings.name) {
+                          case '/home':
+                            return MaterialPageRoute(
+                                builder: (context) => WillPopWrapper(
+                                      child: HomePage(),
+                                    ));
+                          default:
+                            return MaterialPageRoute(
+                                builder: (context) => Container());
+                        }
+                      },
+                    ),
+                    Center(
+                      child: ListTile(
+                        leading: Icon(Icons.construction),
+                        title: Text(
+                          "Under construction, come back later...",
+                          style: Theme.of(context).textTheme.headline6,
                         ),
-                      );
-                    default:
-                      return MaterialPageRoute(
-                          builder: (context) => Container());
-                  }
-                },
-              ),
-              Center(
-                child: Text("Profile"),
-              ),
-            ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
@@ -83,20 +117,21 @@ class _MyAppState extends State<MyApp> {
   MaterialColor _createMaterialColor(Color color) {
     List strengths = <double>[.05];
     final swatch = <int, Color>{};
-    final int r = color.red, g = color.green, b = color.blue;
+    final r = color.red, g = color.green, b = color.blue;
 
-    for (int i = 1; i < 10; i++) {
+    for (var i = 1; i < 10; i++) {
       strengths.add(0.1 * i);
     }
-    strengths.forEach((strength) {
-      final double ds = 0.5 - strength;
+    for (final strength in strengths) {
+      final ds = 0.5 - strength;
       swatch[(strength * 1000).round()] = Color.fromRGBO(
         r + ((ds < 0 ? r : (255 - r)) * ds).round(),
         g + ((ds < 0 ? g : (255 - g)) * ds).round(),
         b + ((ds < 0 ? b : (255 - b)) * ds).round(),
         1,
       );
-    });
+    }
+
     return MaterialColor(color.value, swatch);
   }
 }
--- lib/presentation/widgets/willpop_wrapper.dart
+++ lib/presentation/widgets/willpop_wrapper.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
+/// Widget catches back button press and overrides it
+/// Needed for problem with nested navigation,
+/// when back button press exits app
 class WillPopWrapper extends StatelessWidget {
+  /// Widget which is wrapped
   final Widget child;
+
+  /// Global ket of child navigator
   final GlobalKey<NavigatorState> navKey;
 
+  ///
   const WillPopWrapper({Key key, this.child, this.navKey}) : super(key: key);
 
   @override
@@ -11,7 +18,9 @@ class WillPopWrapper extends StatelessWidget {
     return WillPopScope(
         child: child,
         onWillPop: () async {
-          await navKey.currentState.maybePop();
+          print('willpop wrapper');
+          Navigator.of(context).pop();
+          // await navKey.currentState.maybePop();
           return false;
         });
   }
